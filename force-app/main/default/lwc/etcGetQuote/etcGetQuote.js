07fd4fb3edeb4a38ba7ea1d4969d1c78d39d3769
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2026-01-16 12:41:39 -0600

    render orders to create and add messages

--- force-app/main/default/lwc/etcGetQuote/etcGetQuote.js
+++ force-app/main/default/lwc/etcGetQuote/etcGetQuote.js
@@ -1,4 +1,5 @@
 import { LightningElement, api, track } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getQuote from "@salesforce/apex/ETC_GetQuoteWs.getQuote";
 import findQuoteByExternalId from "@salesforce/apex/ETC_SyncQuoteController.findQuoteByExternalId";
 import getAccount from "@salesforce/apex/ETC_SyncQuoteController.getAccount";
@@ -6,12 +7,23 @@ import getOpportunity from "@salesforce/apex/ETC_SyncQuoteController.getOpportun
 import generateRecordLink from "@salesforce/apex/ETC_SyncQuoteController.generateRecordLink";
 import saveQuoteRecord from "@salesforce/apex/ETC_SyncQuoteController.saveQuoteRecord";
 
+function logob(content) {
+  console.log(
+    "==========================+++++++++++++++=========================="
+  );
+  console.log("LOG:", JSON.stringify(content, null, 4));
+  console.log(
+    "==========================+++++++++++++++=========================="
+  );
+}
+
 export default class EtcGetQuote extends LightningElement {
   @api recordId;
   @api objectApiName;
   @track quoteData = {};
   @track opportunityUrl = null;
   @track quoteUrl = null;
+  @track createdOrders = [];
 
   loading = false;
   isUpdate = false;
@@ -22,6 +34,9 @@ export default class EtcGetQuote extends LightningElement {
     this.formRef.reset();
     this.opportunityUrl = null;
     this.quoteUrl = null;
+    this.loading = false;
+    this.isUpdate = false;
+    this.createdOrders = [];
   }
 
   async searchQuote(evt) {
@@ -29,6 +44,14 @@ export default class EtcGetQuote extends LightningElement {
     this.loading = true;
     try {
       const data = Object.fromEntries(new FormData(evt.currentTarget));
+      if (!data.quoteId) {
+        this.showToast({
+          message: "Por favor ingrese el código de la cotización",
+          title: "Datos incompletos",
+          variant: "warning",
+        });
+        return;
+      }
       const serviceQuoteData = await getQuote({ quoteId: data.quoteId });
       const sfQuote = await findQuoteByExternalId({ externalId: data.quoteId });
       this.quoteUrl = await generateRecordLink({ recordId: sfQuote?.Id });
@@ -53,7 +76,7 @@ export default class EtcGetQuote extends LightningElement {
       if (sfQuote) {
         console.log("Guarda la cotización...");
         this.isUpdate = true;
-        await this.saveQuote();
+        await this.saveQuote(true);
       }
     } catch (error) {
       console.log(error.message);
@@ -62,12 +85,40 @@ export default class EtcGetQuote extends LightningElement {
     }
   }
 
-  async saveQuote() {
+  async saveQuote(isUpdate) {
     this.loading = true;
     try {
       const result = await saveQuoteRecord({
         rawNewQuoteData: JSON.stringify(this.quoteData),
       });
+      // logob(result);
+      if (result.error) {
+        this.showToast({
+          message: result.error,
+          title: "A ocurrido un error",
+          variant: "error",
+        });
+      } else if (isUpdate) {
+        this.showToast({
+          message:
+            "La cotización fue actualizada correctamente, debajo del buscador podrás visualizar los enlaces a los registros creados.",
+          title: "Actualización exitosa",
+          variant: "success",
+        });
+      } else if (result.quoteUrl) {
+        this.showToast({
+          message:
+            "La cotización fue creada correctamente, debajo del buscador podrás visualizar los enlaces a los registros creados.",
+          title: "Operación exitosa",
+          variant: "success",
+        });
+      }
+      for await (let createdOrder of JSON.parse(result.createdOrders)) {
+        this.createdOrders.push({
+          link: await generateRecordLink({ recordId: createdOrder.Id }),
+          ...createdOrder,
+        });
+      }
       this.opportunityUrl = result.opportunityUrl;
       this.quoteUrl = result.quoteUrl;
       this.quoteData.data._sfOpp = await getOpportunity({
@@ -84,5 +135,14 @@ export default class EtcGetQuote extends LightningElement {
   async onChangeOpportunity(evt) {
     this.quoteData.data._sfOpp = { Id: evt.detail.recordId };
     this.quoteData.data.sfOppId = evt.detail.recordId;
+  }
+
+  showToast({ title = "Name", message = "Description", variant = "success" }) {
+    const event = new ShowToastEvent({
+      title,
+      message,
+      variant,
+    });
+    this.dispatchEvent(event);
   }
 }
